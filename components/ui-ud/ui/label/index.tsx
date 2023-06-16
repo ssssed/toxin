084075e3beb46916b7a110ed158536d5c991dd11
Igor Koneshov <igorkoneshov@mail.ru>
2023-06-16 16:12:27 +0400

    feat: add customization to Label

--- components/ui-ud/ui/label/index.tsx
+++ components/ui-ud/ui/label/index.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
 import './style.scss';
 
-const Label: FC<Label> = ({ children }) => {
-  return <h3 className='label'>{children}</h3>;
+const Label: FC<Label> = ({ children, className }) => {
+  return <h3 className={`label ${className}`}>{children}</h3>;
 };
 
 export default Label;
