4cf8648dc3c94c8d4f9a7bf1ab3c82233b8aee1b
Igor Koneshov <igorkoneshov@mail.ru>
2023-06-19 20:52:10 +0400

    feat: add induction

--- components/ui-ud/ui/select/index.tsx
+++ components/ui-ud/ui/select/index.tsx
@@ -2,17 +2,35 @@
 
 import Image from 'next/image';
 import './style.scss';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { CSSTransition } from 'react-transition-group';
 
 const Select: FC<Select> = ({ isShow, setShow, children, value }) => {
   const handleToggleList = () => setShow(!isShow);
+
+  const title = useMemo(() => {
+    if (+value === 0) return 'Сколько гостей';
+    if (+value === 1) {
+      return `${+value} гость`;
+    } else if (+value % 10 === 1 && +value !== 11) {
+      return `${+value} гость`;
+    } else if (
+      +value % 10 >= 2 &&
+      +value % 10 <= 4 &&
+      (+value < 10 || +value > 20)
+    ) {
+      return `${+value} гостя`;
+    } else {
+      return `${+value} гостей`;
+    }
+  }, [value]);
+
   return (
     <div
-      className='select'
+      className={`select ${isShow && 'select_open'}`}
       placeholder='Cколько гостей'
     >
-      <p className='select__text'>{value} гостя</p>
+      <p className='select__text'>{title}</p>
       <Image
         src='/accordion.svg'
         className={`select__img ${isShow && 'select__img_active'}`}
--- components/ui-ud/ui/select/option/index.tsx
+++ components/ui-ud/ui/select/option/index.tsx
@@ -5,18 +5,16 @@ import { Label } from '@/components/ui-ud/ui';
 const Option: FC<Option> = ({ children, value, increment, decrement }) => {
   return (
     <div className='select-item'>
-      <p className='select-item__text'>
-        <span>{children}</span>
-      </p>
+      <div className='select-item__text'>{children}</div>
       <button
         className='select-item__button select-item__button_increment'
         onClick={decrement}
       >
         -
       </button>
-      <span className='select-item__number'>
+      <div className='select-item__number'>
         <Label>{value}</Label>
-      </span>
+      </div>
       <button
         className='select-item__button'
         onClick={increment}
