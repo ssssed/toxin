5b0e195930b26daa4788836944ffb268d6f9646c
Igor Koneshov <igorkoneshov@mail.ru>
2023-06-30 00:37:19 +0400

    feat: add box shadow to header

--- components/ui-ud/ui/header/index.tsx
+++ components/ui-ud/ui/header/index.tsx
@@ -7,16 +7,18 @@ import HeaderButtons from './header-buttons';
 const Header = () => {
   return (
     <header className='header'>
-      <Link href='/'>
-        <Image
-          src='/header-logo.svg'
-          alt='logo'
-          width={105}
-          height={40}
-        />
-      </Link>
-      <HeaderNav />
-      <HeaderButtons />
+      <div className='header__content'>
+        <Link href='/'>
+          <Image
+            src='/header-logo.svg'
+            alt='logo'
+            width={105}
+            height={40}
+          />
+        </Link>
+        <HeaderNav />
+        <HeaderButtons />
+      </div>
     </header>
   );
 };
