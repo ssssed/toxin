8b4a4084513a06643eb7594a4145e6050689ae2c
Igor Koneshov <igorkoneshov@mail.ru>
2023-06-30 00:37:53 +0400

    feat: add padding to rooms page

--- app/rooms/page.tsx
+++ app/rooms/page.tsx
@@ -1,20 +1,24 @@
-"use client";
+'use client';
 
-import {useSession} from "next-auth/react";
-import {Hotels} from "@/components/search/ui";
-import {Container} from "@/components/ui-ud/ui";
+import { useSession } from 'next-auth/react';
+import { Hotels } from '@/components/search/ui';
+import { Container } from '@/components/ui-ud/ui';
+import './style.scss';
 
 export default function Rooms() {
-    const {data: session} = useSession();
-    console.log(session?.user.email, session?.user.lastname);
-    return (
-        <main className="page">
-            <Container direction="row" gap={60} maxWidth={1160} center>
-                <Container maxWidth={266}>
-                    Left Bar
-                </Container>
-                <Hotels/>
-            </Container>
-        </main>
-    );
+  const { data: session } = useSession();
+  console.log(session?.user.email, session?.user.lastname);
+  return (
+    <main className='page rooms-page'>
+      <Container
+        direction='row'
+        gap={60}
+        maxWidth={1160}
+        center
+      >
+        <Container maxWidth={266}>Left Bar</Container>
+        <Hotels />
+      </Container>
+    </main>
+  );
 }
