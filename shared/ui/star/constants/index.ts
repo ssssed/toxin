1bc39d25cf167dbf2560ffc54e240aaf5fbe07f8
Igor Koneshov <igorkoneshov@mail.ru>
2023-07-15 22:11:47 +0400

    feat: decompose star array

---
+++ shared/ui/star/constants/index.ts
@@ -0,0 +1,1 @@
+export const STARS = [1, 2, 3, 4, 5]
--- shared/ui/star/ui.tsx
+++ shared/ui/star/ui.tsx
@@ -1,16 +1,23 @@
 import { FC } from 'react';
 import './style.scss';
 import type { StarType } from './types';
+import { STARS } from './constants';
 
 const Star: FC<StarType> = ({ rating }) => {
   return (
     <div className='stars'>
-      {[...Array(5)].map((_, star) => (
-        <span
-          key={star}
-          className={`star ${star <= rating ? 'star_active' : ''}`}
-        ></span>
-      ))}
+      {STARS.map(star => {
+        let dynamicClass = `star`;
+        if (star <= rating) {
+          dynamicClass = `star star_active`;
+        }
+        return (
+          <span
+            key={star}
+            className={dynamicClass}
+          ></span>
+        );
+      })}
     </div>
   );
 };
