fbc5afd8e53e6339b6fa4d951da9ba2d3452b139
Igor Koneshov <igorkoneshov@mail.ru>
2023-08-17 17:30:08 +0400

    fix: select animate freezes

--- shared/ui/select/ui.tsx
+++ shared/ui/select/ui.tsx
@@ -10,28 +10,32 @@ const Select: FC<SelectType> = ({ isShow, setShow, children, title }) => {
 
   return (
     <div
-      className={`select ${isShow && 'select_open'}`}
+      className='select'
       placeholder={title}
     >
-      <p className='select__text'>{title}</p>
-      <Image
-        src={accordion}
-        className={`select__img ${isShow && 'select__img_active'}`}
-        alt='accordion'
-        onClick={handleToggleList}
-        width={12}
-        height={12}
-      />
-      <CSSTransition
-        in={isShow}
-        classNames='select__animate'
-        timeout={200}
-        unmountOnExit
+      <div
+        className={`select__container ${isShow && 'select__container_open'}`}
       >
-        <div className='select__list'>
-          <div className='select__content'>{children}</div>
-        </div>
-      </CSSTransition>
+        <p className='select__text'>{title}</p>
+        <Image
+          src={accordion}
+          className={`select__img ${isShow && 'select__img_active'}`}
+          alt='accordion'
+          onClick={handleToggleList}
+          width={12}
+          height={12}
+        />
+        <CSSTransition
+          in={isShow}
+          classNames='select__animate'
+          timeout={200}
+          unmountOnExit
+        >
+          <div className='select__list'>
+            <div className='select__content'>{children}</div>
+          </div>
+        </CSSTransition>
+      </div>
     </div>
   );
 };
